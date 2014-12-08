8c08b0c1de0547780ae7fe18ac6374b878be3e06
jasleen <jasleen@jasleen-Inspiron-5521>
2014-12-07 20:27:57 -0800

    Commit

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
+var http=require('http');
 // configure app
 app.use(bodyParser.urlencoded({
   extended: true
